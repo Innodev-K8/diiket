7e36af22a88b0fc979e768dcccec241865942a9a
indmind <sattanationmail@gmail.com>
2021-06-25 15:25:29 +0700

    Update history page

--- lib/ui/pages/main/history/history_page.dart
+++ lib/ui/pages/main/history/history_page.dart
@@ -1,10 +1,33 @@
+import 'package:diiket/ui/common/styles.dart';
 import 'package:flutter/material.dart';
 
 class HistoryPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Text('Ini History Page'),
+      child: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.design_services_rounded,
+              color: ColorPallete.primaryColor,
+              size: 48,
+            ),
+            SizedBox(height: 18),
+            Text(
+              'Akan Datang',
+              style: kTextTheme.headline4,
+            ),
+            SizedBox(height: 6),
+            Text(
+              'Fitur ini masih belum tersedia, tunggu update-update berikutnya ya!',
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }

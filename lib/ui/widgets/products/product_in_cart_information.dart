0eaa909db128b34b05e512369e0ce12ebc15281b
indmind <sattanationmail@gmail.com>
2021-06-25 15:15:37 +0700

    Fix ProductInCartInformation

--- lib/ui/widgets/products/product_in_cart_information.dart
+++ lib/ui/widgets/products/product_in_cart_information.dart
@@ -3,9 +3,10 @@ import 'package:diiket/data/models/product.dart';
 import 'package:diiket/data/providers/order/active_order_provider.dart';
 import 'package:diiket/ui/common/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class ProductInCartInformation extends StatelessWidget {
+class ProductInCartInformation extends HookWidget {
   final Product product;
 
   const ProductInCartInformation({
